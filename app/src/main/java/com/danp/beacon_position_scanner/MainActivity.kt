5d52adca9cb0b50e4fb88549973b37d649f9171e
Paul_Luque <pluquec@unsa.edu.pe>
2024-06-20 19:07:35 -0500

    Creando el proeycto

---
+++ app/src/main/java/com/danp/beacon_position_scanner/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.danp.beacon_position_scanner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.danp.beacon_position_scanner.ui.theme.Beacon_Position_ScannerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Beacon_Position_ScannerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Beacon_Position_ScannerTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/danp/beacon_position_scanner/MyApp.kt
@@ -0,0 +1,8 @@
+package com.danp.beacon_position_scanner
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp: Application() {
+}
